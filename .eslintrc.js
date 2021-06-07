0f1b195586ba072ddd6ac03663a8a0f47cf4de39
Ehsan Sarshar <ehsan.herai@gmail.com>
2021-06-07 15:16:10 +0430

    eslint and prettier parially configured

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: ['react', 'react-native', 'react-hooks', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  root: true,
+  extends: ['@react-native-community', 'prettier'],
+  rules: {
+    'no-semi': 1,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 1,
+    'react-native/no-raw-text': 1,
+    'react-native/no-single-element-style-arrays': 2,
+  },
+}
--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar'
+import React from 'react'
+import {StyleSheet, Text, View} from 'react-native'
 
 export default function App() {
+  const abc = 23
+  useEffect(() => {
+    console.log(abc)
+  }, [])
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +22,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  bb: {
+    backgroundColor: 'red',
+  },
+})
