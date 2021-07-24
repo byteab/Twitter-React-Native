f89fb35432c637a9f1dd9c3ae004decaeb22069c
Ehsan Sarshar <ehsan.herai@gmail.com>
2021-07-24 16:38:30 +0430

    zustand added

--- src/Components/Center.tsx
+++ src/Components/Center.tsx
@@ -2,26 +2,19 @@ import React from 'react'
 import {View, Text, StyleSheet, FlatList} from 'react-native'
 import {CreateTweet, CreateTweetTopRow} from '../Components/CreateTweet'
 import {Post} from '../Components/Post'
+import {IPost, useStore} from '../store/store'
 import {colors} from '../styles/colors'
 
 export function Center() {
-  const data = [
-    {
-      fullName: 'Ehsan Sarshar',
-      userNameAndDate: '@TheEhsanSarshar . 10m',
-      profileUrl:
-        'https://pbs.twimg.com/profile_images/1389235685345959942/B1yoUQGj_400x400.jpg',
-      content: 'Javascript is every where here is a post about it',
-    },
-  ]
+  const posts = useStore((state) => state.posts)
 
   const renderItem = React.useCallback(({item}) => <Post {...item} />, [])
   return (
     <View style={styles.container}>
       <CreateTweetTopRow />
       <CreateTweet style={styles.createTweet} />
-      <FlatList
-        data={data}
+      <FlatList<IPost>
+        data={posts}
         renderItem={renderItem}
         style={styles.flatListStyle}
       />
